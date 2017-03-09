91bca97c3402638414a5478759a6705094b2b0d6
CheongHiuwaa <cheonghiuwaa@gmail.com>
2017-03-09 15:44:17 -0600

    #

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/") t)
@@ -31,5 +38,10 @@
 (setq verilog-auto-endcomments nil)
 
 ;; indentation
+
 (setq-default indent-tabs-mode nil)
 (setq-default c-basic-offset 4)
+(c-set-offset 'case-label '+)
+
+;; display line number
+(global-linum-mode t)
