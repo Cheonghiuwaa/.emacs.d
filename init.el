19c2f1c73ec5702d364e89dfa3987aafc3d5ffbb
Hiuwaa Cheong <cheonghiuwaa@gmail.com>
2017-03-21 05:34:24 -0500

    #

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
@@ -13,13 +12,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+ '(custom-safe-themes (quote ("9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" default)))
+ '(menu-bar-mode nil))
 
 (setq make-backup-files nil) ; stop creating backup~ files
 (setq auto-save-default nil) ; stop creating #autosave# files
@@ -37,11 +31,27 @@
 (setq verilog-auto-newline nil)
 (setq verilog-auto-endcomments nil)
 
-;; indentation
 
+;; indentation
+(electric-indent-mode 1)
+;; .h file -> C++ style
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+(setq c-set-style "K&R")
 (setq-default indent-tabs-mode nil)
 (setq-default c-basic-offset 4)
 (c-set-offset 'case-label '+)
+(c-set-offset 'member-init-intro '++)
+(c-set-offset 'member-init-cont '++)
+(c-set-offset 'access-label '/)
 
 ;; display line number
 (global-linum-mode t)
+(setq linum-format "%d ")
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(region ((t (:background "brightblack")))))
